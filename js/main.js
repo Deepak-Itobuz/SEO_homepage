b6544b292e16675721e29501ced1ba6d118c3d2d
Deepak Patra <deepak@itobuz.com>
2023-02-20 18:23:50 +0530

    20 Feb

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
 let card = document.querySelectorAll(".slider-card")
 let circle = document.querySelectorAll(".circle")
+let price = document.querySelectorAll(".plans")
 
-let i = 0;
+// let i = 0;
 
 // setInterval(() => {
 //     if (i < card.length - 1) {
@@ -20,20 +21,52 @@ let i = 0;
 //     }
 // }, 1500)
 
+price.forEach ((ele) => {
+  ele.addEventListener("mouseover", () =>{
+   ele.firstElementChild.firstElementChild.style.color= "white";
+   ele.lastElementChild.style.backgroundColor= "#FAD725";
+  } )
+} ) 
+price.forEach ((ele) => {
+  ele.addEventListener("mouseout", () =>{
+   ele.firstElementChild.firstElementChild.style.color= "#6f77f1";
+   ele.lastElementChild.style.backgroundColor= "white";
+  } )
+} ) 
 
 
+// var swiper = new Swiper(".mySwiper", {
+//     slidesPerView: 3,
+//     centeredSlides: true,
+//     spaceBetween: 30,
+//     grabCursor: true,
+//     autoplay: {
+//         delay: 2500,
+//         disableOnInteraction: false,
+//       },
+//     // freeMode:true,
+//     pagination: {
+//       el: ".swiper-pagination",
+//       clickable: true,
+//     },
+//   });
 var swiper = new Swiper(".mySwiper", {
-    slidesPerView: "auto",
-    centeredSlides: true,
-    spaceBetween: 30,
-    grabCursor: true,
-    autoplay: {
-        delay: 2500,
-        disableOnInteraction: false,
-      },
-    // freeMode:true,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
+  slidesPerView: 1,
+  spaceBetween: 20,
+  centeredSlides: true,
+  freeMode: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    650: {
+      slidesPerView: 3,
+      spaceBetween: 30,
     },
-  });
+  },
+})
